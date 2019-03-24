f4061b94a127c0605da41211e7a5a247c55c7eb3
aussftw <shagronaus@gmail.com>
2019-03-24 16:14:37 +0200

    footer styles finished

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -4,35 +4,25 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer>
-      <div class="contact-body" id="contact">
-        <h2>Contact me</h2>
-        <p>Get in touch with me</p>
-        <div class="container">
-          <div class="col-left">
-            <div class="socials">
-              <a class="btn" href="https://github.com/aussftw">
-                <i class="fab fa-github" />
+      <div className="footer-container">
+        <div className="footer-text">
+          Developed and Made with
+          <span className="brown"> &#x2665;</span>
+          <div className="footer-text-extented">
+            by Alexander Kaminskiy &copy; 2019
+            <span className="socials">
+              <a className="btn" href="https://github.com/aussftw">
+                <i className="github icon" />
               </a>
               <a
-                class="btn"
+                className="btn"
                 href="https://www.linkedin.com/in/alexander-k-7a7a32165/"
               >
-                <i class="fab fa-linkedin-in" />
+                <i className="linkedin icon" />
               </a>
-              <a class="btn" href="https://www.instsagram.com/auscapulet">
-                <i class="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <div class="col-right">
-            <div class="contacts-container" />
+            </span>
           </div>
         </div>
-      </div>
-      <div className="footer-text">
-        Developed and Made with
-        <span className="brown">&#x2665;</span>
-        <div>by Alexander Kaminskiy &copy; 2019</div>
       </div>
     </footer>
   );
